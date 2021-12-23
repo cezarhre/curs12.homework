fcd98dd7e52b59a512862438c93b97a5e531a07a
cezar <cezar_h@yahoo.com>
2021-12-23 23:20:07 +0200

    Am facut numai in main... cred ca e mai ok asa

--- src/ro/fasttrackit/curs12/homework/ex1/StudentMain.java
+++ src/ro/fasttrackit/curs12/homework/ex1/StudentMain.java
@@ -1,12 +1,12 @@
 package ro.fasttrackit.curs12.homework.ex1;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
         public class StudentMain {
             public static void main(String[] args) {
                 Map<String, Integer> students = new HashMap<>();
+
                 students.put("Ionel", 10);
                 students.put("Costel", 8);
                 students.put("Vasile", 7);
@@ -14,11 +14,13 @@ import java.util.Map;
 
                 System.out.println(students);
 
-                Student student = new Student(students);
-                System.out.println(student.getMaxGrade(students));
-
-
-
+                Map.Entry<String, Integer> maxGrade = null;
+               for(Map.Entry<String, Integer> stud : students.entrySet()){
+                   if(maxGrade==null||stud.getValue().compareTo(maxGrade.getValue())>0){
+                       maxGrade = stud;
+                   }
+               }
+                System.out.println(maxGrade);
 
             }
 }
