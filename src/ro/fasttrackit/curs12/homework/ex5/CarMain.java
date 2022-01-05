381aab1a0158ca488477ebeeba51ef4d2eb71063
cezar <cezar_h@yahoo.com>
2022-01-05 22:27:04 +0200

    Exercitiul 5 update

--- src/ro/fasttrackit/curs12/homework/ex5/CarMain.java
+++ src/ro/fasttrackit/curs12/homework/ex5/CarMain.java
@@ -5,9 +5,11 @@ import java.util.List;
 public class CarMain {
     public static void main(String[] args) {
         CarShop carShop = new CarShop(List.of(
-                new Car("Volvo",8,150000,10000),
-                new Car("Dacia",5,120000,4500),
+                new Car("Volvo",8,150000,9000),
+                new Car("Dacia",5,70000,6000),
+                new Car("Ford",7,120000,9500),
                 new Car("Toyota",5,75000,12000),
+                new Car("Dacia",9,65000,4500),
                 new Car("Ford",6,95000,11000)
         ));
 
--- src/ro/fasttrackit/curs12/homework/ex5/CarShop.java
+++ src/ro/fasttrackit/curs12/homework/ex5/CarShop.java
@@ -15,51 +15,56 @@ public class CarShop {
     public Map<String, Integer> mapNameNumber() {
         Map<String, Integer> result = new HashMap<>();
         for (Car car : carShop) {
-             result.put(car.getName(), carShop.size());
-           }
+             Integer count = result.get(car.getName());
+                result.put(car.getName(), count == null ? 1 : count + 1);
+            }
         return result;
     }
 
     public Map<String, Integer> mapNameKm() {
         Map<String, Integer> result = new HashMap<>();
-        int sum = 0;
         for (Car car : carShop) {
-            sum += car.getKm();
-        }
-        for (Car car : carShop){                    // stiu ca nu arata bine dar nu am gasit alta solutie...
-            result.put(car.getName(), sum);
+            Integer count = result.get(car.getName());
+            result.put(car.getName(), count == null ? car.getKm() : count + car.getKm());
         }
         return result;
     }
 
-    public Map<Integer, List<Car>> mapKmCar() {
-        Map<Integer, List<Car>> range = new HashMap<>();
-        for (Car car : carShop) {
+    public Map<CarRange, List<Car>> mapKmCar() {
+        List<CarRange> rangeKm = new ArrayList<>();
+        rangeKm.add(new CarRange(50000, 80000));
+        rangeKm.add(new CarRange(80001, 100000));
+        rangeKm.add(new CarRange(100001, 150000));
+        Map<CarRange, List<Car>> range = new HashMap<>();
+        for (CarRange range1 : rangeKm) {
             List<Car> carShopList = new ArrayList<>();
-            if (car.getKm() > 1 && car.getKm() < 100000) {
-                range.put(car.getKm(), carShopList);
+            for (Car car : carShop) {
+                if (car.getKm() > range1.minRange && car.getKm() < range1.maxRange) {
+                    carShopList.add(car);
+                }
             }
-            carShopList.add(car);
+                range.put(range1, carShopList);
+            }
+            return range;
+        }
+
+    public Map<CarRange, List<Car>> mapPriceCar(){
+        List<CarRange> rangePrice = new ArrayList<>();
+        rangePrice.add(new CarRange(2000, 5000));
+        rangePrice.add(new CarRange(5001, 10000));
+        rangePrice.add(new CarRange(10001, 15000));
+        Map<CarRange, List<Car>> range = new HashMap<>();
+        for (CarRange range1 : rangePrice) {
+            List<Car> carShopList = new ArrayList<>();
+            for (Car car : carShop) {
+                if (car.getPrice() > range1.minRange && car.getPrice() < range1.maxRange) {
+                    carShopList.add(car);
+                }
+            }
+            range.put(range1,carShopList);
         }
         return range;
     }
-
-    public Map<Integer, List<Car>> mapPriceCar(){
-        Map<Integer, List<Car>> range = new HashMap<>();
-        for(Car car : carShop){
-            List<Car> carShopList = new ArrayList<>();
-            if(car.getPrice() > 1 && car.getPrice() < 10000){
-            range.put(car.getPrice(), carShopList);
-            }
-            carShopList.add(car);
-        }
-        return range;
 }
 
-    @Override
-    public String toString() {
-        return "CarShop{" +
-                "carShop=" + carShop +
-                '}';
-    }
-}
+
